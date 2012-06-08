a53071d82a5a85b4e349ae96ecaee6c16c6e85d3
visnupriya <visnupriya.be@gmail.com>
2012-06-08 17:21:02 +0530

    added footer

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,11 @@ class AppApplication < Rho::RhoApplication
     # Super must be called *after* settings @tabs!
     @tabs = nil
     #To remove default toolbar uncomment next line:
-    #@@toolbar = nil
+    @@toolbar = nil
+    @default_menu = {
+         "Dashboard" => "/app/RhoMonitor/dashboard", 
+         "Close" => :close
+       }
     super
     # Uncomment to set sync notification callback to /app/Settings/sync_notify.
     # SyncEngine::set_objectnotify_url("/app/Settings/sync_notify")
